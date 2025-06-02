3d4c7e9567c6cd3f4a0095454be39dbe51c6340a
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2025-06-02 15:42:27 +0300

    add CommunicationControl docker integration

--- src/CommunicationControl/DevOpsProject/DI/RedisConfiguration.cs
+++ src/CommunicationControl/DevOpsProject/DI/RedisConfiguration.cs
@@ -10,14 +10,17 @@ namespace DevOpsProject.CommunicationControl.API.DI
         public static IServiceCollection AddRedis(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
             var redisConfiguration = configuration.GetSection("Redis").Get<RedisOptions>();
-            var redisEnvironmentConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING") ?? redisConfiguration.ConnectionString;
+            var redisEnvironmentConnectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? redisConfiguration.ConnectionString;
+            System.Console.WriteLine($"Using Redis connection string from digital ocean: {redisEnvironmentConnectionString}");
 
             var options = new ConfigurationOptions
             {
                 EndPoints = { redisEnvironmentConnectionString ?? redisConfiguration.ConnectionString },
-                Ssl = true
+                Ssl = true,
+                AbortOnConnectFail = false
             };
             var redis = ConnectionMultiplexer.Connect(options);
+            // var redis = ConnectionMultiplexer.Connect(redisConfiguration.ConnectionString);
             
             serviceCollection.AddSingleton<IConnectionMultiplexer>(redis);
 
